b123ad73b34d05df96f92ba5363d985cd86f3680
gayanyapa <gayan.yapa@mybudget.com.au>
2023-06-19 20:09:12 +0930

    Int

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/gmbyapa/fk1/sub2"
+import "fk1/sub2"
 
 func main() {
 	sub2.Fn2()
--- sub2/fn.go
+++ sub2/fn.go
@@ -1,6 +1,6 @@
 package sub2
 
-import "github.com/gmbyapa/fk1/sub"
+import "fk1/sub"
 
 func Fn2() {
 	sub.Fn1()
